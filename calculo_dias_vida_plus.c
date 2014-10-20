d13bf0982ae888ebb4147afe8342d0204cf56a26
Erick Alves <erickalvessud@gmail.com>
2014-10-20 17:26:48 -0200

    Algoritmo Calculo dias de vida

---
+++ calculo_dias_vida_plus.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+
+/*c) Ler a idade de uma pessoa. Calcular e apresentar a existencia desta pessoa em
+numero de dias, em numero de horas, em numero de minutos e em numero de
+segundos.*/
+int main(){
+	
+	int idade = 0;
+	
+	int dia = 0;
+	int hora = 0;
+	int minuto = 0;
+	int segundo = 0;
+	char data[] =  "02-08-2014";
+	char *a;
+	
+	a = strtok(data, "-");
+/*
+	while(a){
+		printf("Data: %s \n", a);
+		a = strtok(NULL, "-");
+	}
+*/	
+	printf("Quantos dias de vida voce tem?\n");
+	printf("Vamos comecar. Qual eh a data de aniversario?\n");
+	scanf("%d", &idade);
+	
+	dia 	= idade * 365;
+	hora 	= dia * 24;
+	minuto 	= hora * 60;
+	segundo	= minuto * 60;
+	
+	printf("Você tem %d dias, %d horas, %d minutos, %d segundos de vida.\n", dia, hora, minuto, segundo);
+	
+	system("pause");
+	return 0;
+}
