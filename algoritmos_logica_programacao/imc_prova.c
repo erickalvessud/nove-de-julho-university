e1a59bc579a465f9280062054d340148fc7d2df3
Erick Alves <erickalvessud@gmail.com>
2014-10-29 11:42:59 -0200

    Melhorei a eficiencia e a legibilidade do codigo colocando apenas um laco while.

--- algoritmos_logica_programacao/imc_prova.c
+++ algoritmos_logica_programacao/imc_prova.c
@@ -12,13 +12,14 @@ int main(){
     float taxaUsuario = 0;
     
 	// peso ideal de uma pessoa normal.
-    float pesoIdeal = 0;
-    float pesoDiff = 0;
+    float massaIdeal = 0;
+    float massaDiff = 0;
     // meses que o usuario precisa para perder ou ganhar peso para chegar ao seu peso ideal.
     int mes = 0;
     
     float ideal = 0;
     
+    printf("Calcular o IMC\n\n");
     printf("Digite seu peso:\n");
     scanf("%f", &massa);
     
@@ -47,27 +48,60 @@ int main(){
 	if (ideal == 0) {
 
 		if (imc < 18.5) {
-          // ele esta abaixo do peso
-          
-          	pesoIdeal = 18.5 * (pow(altura, 2));
+			// ele esta abaixo do peso          
+          	massaIdeal = 18.5 * (pow(altura, 2));
                 
        	} else {
-         	// ele acima do peso
-         
-          	pesoIdeal = 24.99 * (pow(altura, 2));         
+         	// ele acima do peso    
+          	massaIdeal = 24.99 * (pow(altura, 2));         
        	}
 
-       	pesoDiff = massa - pesoIdeal;
+       	massaDiff = massa - massaIdeal;
+	   	
+	   	if (massaDiff > 0){
+			printf("Para sua altura, seu peso ideal eh %.2f Voce precisa perder: %.2fkg\n", massaIdeal, massaDiff);
+	        printf("Quantos por cento voce consegue perder por mes?\n");
+	        scanf("%f", &taxaUsuario); 
+		} else {
+			printf("Seu peso ideal eh %.2f Voce precisa ganhar: %.2f\n", massaIdeal, (massaDiff * -1)); // multiplico por -1 para tirar o sinal negativo
+	        printf("Quantos por cento voce consegue ganhar por mes?\n");
+	        scanf("%f", &taxaUsuario);
+		}
 	   
 	   	float result = 0;
-	   	
-		if (pesoDiff > 0) {
-	    	
-	    	printf("Para sua altura, seu peso ideal eh %.2f Voce precisa perder: %.2fkg\n", pesoIdeal, pesoDiff);
+		
+		// simplifiquei colocando apenas um laco de repeticao.
+		// se o diferenca entre a massa atual e a massa normal for positivo E a massa atual for maior que a massa ideal OU se a diferenca entre a massa atual e a massa normal for negativa e a massa atual for menor que a massa ideal, continua o laco.
+		// Quando essa condicao nao for mais satisfeita, ele termina a iteracao do laco.
+		while ( (massaDiff > 0 && massa > massaIdeal) || (massaDiff < 0 && massa < massaIdeal)){
+			
+			// descobrir o resultado da porcentagem sobre a massa atual.
+			result = massa * (taxaUsuario/100);
+			
+			if (massaDiff > 0){
+				// diminue da massa atual a porcentagem encontrada
+				massa = massa - result;
+			} else {
+				// aumenta da massa atual a porcentagem encontrada
+				massa = massa + result;
+			}
+			// calcula para saber o novo IMC da massa nova.
+			imc = massa / (pow(altura, 2));
+			
+			// incrementa o contador de meses
+	        mes++;
+	        
+			printf("No %d mes, sua massa sera %.2f, seu IMC: %.2f\n", mes, massa, imc);
+		}
+		
+	    /*	
+	    //logica antiga
+	    
+	    	printf("Para sua altura, seu peso ideal eh %.2f Voce precisa perder: %.2fkg\n", massaIdeal, massaDiff);
 	        printf("Em porcentagem, quanto voce consegue perder\n");
 	        scanf("%f", &taxaUsuario);                 
 	        
-	        while( massa > pesoIdeal){
+	        while( massa > massaIdeal){
 	             result = massa * (taxaUsuario/100);
 	             massa = massa - result;
 	             imc = massa / (pow(altura, 2));
@@ -77,11 +111,11 @@ int main(){
 	        }
 	    } else {
 	    	
-			printf("Seu peso ideal eh %.2f Voce precisa ganhar: %.2f\n", pesoIdeal, pesoDiff);
+			printf("Seu peso ideal eh %.2f Voce precisa ganhar: %.2f\n", massaIdeal, massaDiff);
 	        printf("Em porcentagem, quanto voce consegue ganhar\n");
 	        scanf("%f", &taxaUsuario);                 
 	        
-	        while( massa < pesoIdeal){
+	        while( massa < massaIdeal){
 	             result = massa * (taxaUsuario/100);
 	             massa = massa + result;               
 	             imc = massa / (pow(altura, 2));
@@ -89,7 +123,7 @@ int main(){
 	             printf("Sua massa eh %.2f, seu IMC: %.2f, em %d\n", massa, imc, mes);
 	        }
 	    }
-	          
+	    */
     } else {
     	printf("Voce esta entre o IMC ideal. Seu IMC eh %.2f\n", imc);
     }
